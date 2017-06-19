e1b46c87aeb20fcb0a460372de2fcd63e2530f04
jjuiddong <jjuiddong@gmail.com>
2017-06-19 19:30:24 +0900

    update Pathfinder, manhatan distance test

--- Src/Test/PathFinder/PathFinder/pathfindertest.cpp
+++ Src/Test/PathFinder/PathFinder/pathfindertest.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "stdafx.h"
+#include "../../../../../Common/AI/path/pathfinder.h"
 
 using namespace graphic;
 
@@ -24,6 +25,18 @@ public:
 
 public:
 	sf::Vector2i m_curPos;
+	float m_moveLen;
+	Plane m_groundPlane1;
+	cGrid m_ground;
+	cModel2 m_model;
+	cDbgBox m_box;
+	cDbgLine m_line;
+	Vector3 m_startPos;
+	Vector3 m_endPos;
+
+	ai::cPathFinder m_pathFinder;
+
+
 	bool m_LButtonDown;
 	bool m_RButtonDown;
 	bool m_MButtonDown;
@@ -33,13 +46,13 @@ INIT_FRAMEWORK(cViewer);
 
 
 cViewer::cViewer()
-//	: m_groundPlane1(Vector3(0, 1, 0), 0)
+	: m_groundPlane1(Vector3(0, 1, 0), 0)
 {
 	m_windowName = L"PathFinder Test";
 	//const RECT r = { 0, 0, 1024, 768 };
 	const RECT r = { 0, 0, 1280, 1024 };
 	m_windowRect = r;
-	//m_moveLen = 0;
+	m_moveLen = 0;
 	m_LButtonDown = false;
 	m_RButtonDown = false;
 	m_MButtonDown = false;
@@ -60,7 +73,7 @@ bool cViewer::OnInit()
 	const int WINSIZE_X = m_windowRect.right - m_windowRect.left;
 	const int WINSIZE_Y = m_windowRect.bottom - m_windowRect.top;
 	GetMainCamera()->Init(&m_renderer);
-	GetMainCamera()->SetCamera(Vector3(30, 30, -30), Vector3(0, 0, 0), Vector3(0, 1, 0));
+	GetMainCamera()->SetCamera(Vector3(30, 10, -30), Vector3(0, 0, 0), Vector3(0, 1, 0));
 	GetMainCamera()->SetProjection(D3DX_PI / 4.f, (float)WINSIZE_X / (float)WINSIZE_Y, 0.1f, 10000.0f);
 	GetMainCamera()->SetViewPort(WINSIZE_X, WINSIZE_Y);
 
@@ -78,8 +91,73 @@ bool cViewer::OnInit()
 	//m_gui.Init(m_hWnd, m_renderer.GetDevice());
 	//m_gui.SetContext();
 
-	//m_ground.Create(m_renderer, 10, 10, 100, 200);
+	m_model.Create(m_renderer, 0, "ChessBishop.x");
+	Matrix44 S;
+	S.SetScale(Vector3(80, 80, 80));
+	m_model.m_tm = S;
+
+	m_box.InitBox(m_renderer);
+	m_box.SetColor(0);
+	m_line.InitCube(m_renderer);
+
+	m_ground.Create(m_renderer, 10, 10, 100);
 	//m_ground.m_tex = cResourceManager::Get()->LoadTexture(m_renderer, "../media/terrain/¹Ù´Ú.jpg");
+
+	m_pathFinder.Create(100);
+
+	for (int x = 0; x < 5; ++x)
+	{
+		for (int z = 0; z < 5; ++z)
+		{
+			ai::sVertex vtx;
+			vtx.pos = Vector3(x * 10.f, 0, z * 10.f);
+			for (int i = 0; i < 4; ++i)
+				vtx.edge[i] = -1;
+
+			int idx = 0;
+			if (x-1 >= 0)
+				vtx.edge[idx++] = z + (x-1)*5;
+			if (x + 1 < 5)
+				vtx.edge[idx++] = z + (x + 1) * 5;
+			if (z - 1 >= 0)
+				vtx.edge[idx++] = z-1 + x*5;
+			if (z + 1 < 5)
+				vtx.edge[idx++] = z+1 + x*5;
+
+			m_pathFinder.AddVertex(vtx);
+		}
+	}
+
+
+	for (int x = 0; x < 5; ++x)
+	{
+		for (int z = 0; z < 5; ++z)
+		{
+			ai::sVertex vtx;
+			vtx.pos = Vector3(x * 10.f, 0, z * 10.f) + Vector3(100,0,100);
+			for (int i = 0; i < 4; ++i)
+				vtx.edge[i] = -1;
+
+			int idx = 0;
+			if (x - 1 >= 0)
+				vtx.edge[idx++] = z + (x - 1) * 5 + 25;
+			if (x + 1 < 5)
+				vtx.edge[idx++] = z + (x + 1) * 5 + 25;
+			if (z - 1 >= 0)
+				vtx.edge[idx++] = z - 1 + x * 5 + 25;
+			if (z + 1 < 5)
+				vtx.edge[idx++] = z + 1 + x * 5 + 25;
+
+			m_pathFinder.AddVertex(vtx);
+		}
+	}
+
+	m_pathFinder.m_vertices[24].edge[2] = 25;
+	m_pathFinder.m_vertices[25].edge[2] = 24;
+
+
+	m_startPos = Vector3(0, 0, 0);
+	m_endPos = Vector3(90, 0, 40);
 
 	return true;
 }
@@ -87,30 +165,29 @@ bool cViewer::OnInit()
 
 void cViewer::OnUpdate(const float deltaSeconds)
 {
+	m_model.Update(m_renderer, deltaSeconds);
+
 	// keyboard
 	if (GetFocus() == m_hWnd)
 	{
 		const float vel = 10 * deltaSeconds;
-		//if (GetAsyncKeyState('W'))
-		//	GetMainCamera()->MoveFront(vel);
-		//else if (GetAsyncKeyState('A'))
-		//	GetMainCamera()->MoveRight(-vel);
-		//else if (GetAsyncKeyState('D'))
-		//	GetMainCamera()->MoveRight(vel);
-		//else if (GetAsyncKeyState('S'))
-		//	GetMainCamera()->MoveFront(-vel);
-		//else if (GetAsyncKeyState('E'))
-		//	GetMainCamera()->MoveUp(vel);
-		//else if (GetAsyncKeyState('C'))
-		//	GetMainCamera()->MoveUp(-vel);
+		if (GetAsyncKeyState('W'))
+			GetMainCamera()->MoveFront(vel);
+		else if (GetAsyncKeyState('A'))
+			GetMainCamera()->MoveRight(-vel);
+		else if (GetAsyncKeyState('D'))
+			GetMainCamera()->MoveRight(vel);
+		else if (GetAsyncKeyState('S'))
+			GetMainCamera()->MoveFront(-vel);
+		else if (GetAsyncKeyState('E'))
+			GetMainCamera()->MoveUp(vel);
+		else if (GetAsyncKeyState('C'))
+			GetMainCamera()->MoveUp(-vel);
 
 		GetMainCamera()->Update(deltaSeconds);
 	}
 }
 
-bool show_test_window = true;
-bool show_another_window = false;
-ImVec4 clear_col = ImColor(114, 144, 154);
 
 void cViewer::OnRender(const float deltaSeconds)
 {
@@ -120,7 +197,33 @@ void cViewer::OnRender(const float deltaSeconds)
 		m_renderer.BeginScene();
 		m_renderer.GetDevice()->SetTransform(D3DTS_WORLD, (D3DMATRIX*)&Matrix44::Identity);
 
-	m_renderer.EndScene();
+		GetMainCamera()->Bind(*m_model.m_shader);
+		GetMainLight().Bind(*m_model.m_shader);
+
+		m_ground.Render(m_renderer);
+
+		for (auto &vtx : m_pathFinder.m_vertices)
+		{
+			Matrix44 tm;
+			tm.SetPosition(vtx.pos);
+			m_box.Render(m_renderer, tm);
+		}
+
+		vector<Vector3> path;
+		m_pathFinder.Find(m_startPos, m_endPos, path);
+		for (u_int i = 1; i < path.size(); ++i)
+		{
+			m_line.SetLine(path[i - 1], path[i], 0.1f);
+			m_line.Render(m_renderer);
+
+		}
+
+		m_model.RenderShader(m_renderer);
+
+		m_renderer.RenderFPS();
+		m_renderer.RenderAxis();
+
+		m_renderer.EndScene();
 		m_renderer.Present();
 	}
 }
@@ -216,11 +319,23 @@ void cViewer::OnMessageProc(UINT message, WPARAM wParam, LPARAM lParam)
 		m_curPos.x = LOWORD(lParam);
 		m_curPos.y = HIWORD(lParam);
 
-		//Vector3 orig, dir;
-		//graphic::GetMainCamera()->GetRay(pos.x, pos.y, orig, dir);
-		//Vector3 p1 = m_groundPlane1.Pick(orig, dir);
-		//m_moveLen = common::clamp(1, 100, (p1 - orig).Length());
-		//graphic::GetMainCamera()->MoveCancel();
+		Vector3 orig, dir;
+		graphic::GetMainCamera()->GetRay(pos.x, pos.y, orig, dir);
+		Vector3 p1 = m_groundPlane1.Pick(orig, dir);
+		m_moveLen = common::clamp(1, 100, (p1 - orig).Length());
+		graphic::GetMainCamera()->MoveCancel();
+
+		for (auto &vtx : m_pathFinder.m_vertices)
+		{
+			cBoundingBox bbox;
+			bbox.SetBoundingBox(vtx.pos - Vector3(1, 1, 1)
+				, vtx.pos + Vector3(1, 1, 1));
+			if (bbox.Pick(orig, dir))
+			{
+				m_startPos = vtx.pos;
+				break;
+			}
+		}
 	}
 	break;
 
@@ -239,6 +354,21 @@ void cViewer::OnMessageProc(UINT message, WPARAM wParam, LPARAM lParam)
 		Ray ray(m_curPos.x, m_curPos.y, 1024, 768,
 			GetMainCamera()->GetProjectionMatrix(),
 			GetMainCamera()->GetViewMatrix());
+
+		Vector3 orig, dir;
+		graphic::GetMainCamera()->GetRay(m_curPos.x, m_curPos.y, orig, dir);
+
+		for (auto &vtx : m_pathFinder.m_vertices)
+		{
+			cBoundingBox bbox;
+			bbox.SetBoundingBox(vtx.pos - Vector3(1, 1, 1)
+				, vtx.pos + Vector3(1, 1, 1));
+			if (bbox.Pick(orig, dir))
+			{
+				m_endPos = vtx.pos;
+				break;
+			}
+		}
 	}
 	break;
 
@@ -286,8 +416,8 @@ void cViewer::OnMessageProc(UINT message, WPARAM wParam, LPARAM lParam)
 			right.y = 0;
 			right.Normalize();
 
-			//graphic::GetMainCamera()->MoveRight(-x * m_moveLen * 0.001f);
-			//graphic::GetMainCamera()->MoveFrontHorizontal(y * m_moveLen * 0.001f);
+			graphic::GetMainCamera()->MoveRight(-x * m_moveLen * 0.001f);
+			graphic::GetMainCamera()->MoveFrontHorizontal(y * m_moveLen * 0.001f);
 		}
 		else if (m_RButtonDown)
 		{
