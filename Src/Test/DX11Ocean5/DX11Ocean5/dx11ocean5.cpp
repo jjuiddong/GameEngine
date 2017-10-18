322a50b54608f52c6e920c7afcc9ace64668284f
lee jaejung <jjuiddong@gmail.com>
2017-10-19 00:03:39 +0900

    update DX11Ocean5 Sample

---
+++ Src/Test/DX11Ocean5/DX11Ocean5/dx11ocean5.cpp
@@ -0,0 +1,172 @@
+//
+// DX11 NormalMap
+//
+
+#include "stdafx.h"
+#include "dockview.h"
+
+using namespace graphic;
+using namespace framework;
+
+class cViewer : public framework::cGameMain2
+{
+public:
+	cViewer();
+	virtual ~cViewer();
+	virtual bool OnInit() override;
+	virtual void OnUpdate(const float deltaSeconds) override;
+	virtual void OnRender(const float deltaSeconds) override;
+	virtual void OnEventProc(const sf::Event &evt) override;
+};
+
+
+INIT_FRAMEWORK3(cViewer);
+cViewer *g_viewer = NULL;
+
+cViewer::cViewer()
+{
+	m_windowName = L"DX11 Ocean5";
+	//const RECT r = { 0, 0, 1024, 768 };
+	const RECT r = { 0, 0, (int)(1280 * 1.f), (int)(1024 * 1.f) };
+	m_windowRect = r;
+}
+
+cViewer::~cViewer()
+{
+}
+
+
+bool cViewer::OnInit()
+{
+	g_viewer = this;
+
+	DragAcceptFiles(m_hWnd, TRUE);
+
+	cResourceManager::Get()->SetMediaDirectory("../media/");
+
+	m_gui.SetContext();
+
+	cDockView1 *view1 = new cDockView1("Model View1");
+	view1->Create(eDockState::DOCK, eDockSlot::TAB, this, NULL);
+	view1->Init();
+
+	//cDockView1 *view2 = new cDockView1("Light View");
+	//view2->Create(eDockState::DOCK, eDockSlot::BOTTOM, this, view1);
+	//view2->Init();
+
+	//cDockView1 *view3 = new cDockView1("DockView3");
+	//view3->Create(eDockState::DOCK, eDockSlot::RIGHT, this, view2);
+	//view3->Init();
+
+	const int cx = GetSystemMetrics(SM_CXSCREEN);
+	const int cy = GetSystemMetrics(SM_CYSCREEN);
+
+	m_gui.SetContext();
+
+	float col_main_hue = 0.0f / 255.0f;
+	float col_main_sat = 0.0f / 255.0f;
+	float col_main_val = 80.0f / 255.0f;
+
+	float col_area_hue = 0.0f / 255.0f;
+	float col_area_sat = 0.0f / 255.0f;
+	float col_area_val = 50.0f / 255.0f;
+
+	float col_back_hue = 0.0f / 255.0f;
+	float col_back_sat = 0.0f / 255.0f;
+	float col_back_val = 35.0f / 255.0f;
+
+	float col_text_hue = 0.0f / 255.0f;
+	float col_text_sat = 0.0f / 255.0f;
+	float col_text_val = 255.0f / 255.0f;
+	float frameRounding = 0.0f;
+
+	ImVec4 col_text = ImColor::HSV(col_text_hue, col_text_sat, col_text_val);
+	ImVec4 col_main = ImColor::HSV(col_main_hue, col_main_sat, col_main_val);
+	ImVec4 col_area = ImColor::HSV(col_area_hue, col_area_sat, col_area_val);
+	ImVec4 col_back = ImColor::HSV(col_back_hue, col_back_sat, col_back_val);
+	float rounding = frameRounding;
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.FrameRounding = rounding;
+	style.WindowRounding = rounding;
+	style.Colors[ImGuiCol_Text] = ImVec4(col_text.x, col_text.y, col_text.z, 1.00f);
+	style.Colors[ImGuiCol_TextDisabled] = ImVec4(col_text.x, col_text.y, col_text.z, 0.58f);
+	style.Colors[ImGuiCol_WindowBg] = ImVec4(col_back.x, col_back.y, col_back.z, 0.80f);
+	style.Colors[ImGuiCol_ChildWindowBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.00f);
+	style.Colors[ImGuiCol_PopupBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.00f);
+	style.Colors[ImGuiCol_Border] = ImVec4(col_text.x, col_text.y, col_text.z, 0.30f);
+	style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	style.Colors[ImGuiCol_FrameBg] = ImVec4(col_back.x, col_back.y, col_back.z, 1.00f);
+	style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.68f);
+	style.Colors[ImGuiCol_FrameBgActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_TitleBg] = ImVec4(col_main.x, col_main.y, col_main.z, 1.0f);
+	style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(col_main.x, col_main.y, col_main.z, 1.0f);
+	style.Colors[ImGuiCol_TitleBgActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.0f);
+	style.Colors[ImGuiCol_MenuBarBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.0f);
+	style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.00f);
+	style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(col_main.x, col_main.y, col_main.z, 0.31f);
+	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.78f);
+	style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_ComboBg] = ImVec4(col_area.x, col_area.y, col_area.z, 1.00f);
+	style.Colors[ImGuiCol_CheckMark] = ImVec4(col_text.x, col_text.y, col_text.z, 0.80f);
+	style.Colors[ImGuiCol_SliderGrab] = ImVec4(col_main.x, col_main.y, col_main.z, 0.54f);
+	style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_Button] = ImVec4(col_main.x, col_main.y, col_main.z, 0.44f);
+	style.Colors[ImGuiCol_ButtonHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.86f);
+	style.Colors[ImGuiCol_ButtonActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_Header] = ImVec4(col_main.x, col_main.y, col_main.z, 0.76f);
+	style.Colors[ImGuiCol_HeaderHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.86f);
+	style.Colors[ImGuiCol_HeaderActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_Column] = ImVec4(col_text.x, col_text.y, col_text.z, 0.32f);
+	style.Colors[ImGuiCol_ColumnHovered] = ImVec4(col_text.x, col_text.y, col_text.z, 0.78f);
+	style.Colors[ImGuiCol_ColumnActive] = ImVec4(col_text.x, col_text.y, col_text.z, 1.00f);
+	style.Colors[ImGuiCol_ResizeGrip] = ImVec4(col_main.x, col_main.y, col_main.z, 0.20f);
+	style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 0.78f);
+	style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_CloseButton] = ImVec4(col_text.x, col_text.y, col_text.z, 0.16f);
+	style.Colors[ImGuiCol_CloseButtonHovered] = ImVec4(col_text.x, col_text.y, col_text.z, 0.39f);
+	style.Colors[ImGuiCol_CloseButtonActive] = ImVec4(col_text.x, col_text.y, col_text.z, 1.00f);
+	style.Colors[ImGuiCol_PlotLines] = ImVec4(col_text.x, col_text.y, col_text.z, 0.63f);
+	style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_PlotHistogram] = ImVec4(col_text.x, col_text.y, col_text.z, 0.63f);
+	style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(col_main.x, col_main.y, col_main.z, 1.00f);
+	style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(col_main.x, col_main.y, col_main.z, 0.43f);
+	style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.10f, 0.10f, 0.10f, 0.55f);
+
+	GetMainLight().Init(cLight::LIGHT_DIRECTIONAL,
+		Vector4(0.2f, 0.2f, 0.2f, 1), Vector4(0.9f, 0.9f, 0.9f, 1),
+		Vector4(0.2f, 0.2f, 0.2f, 1));
+	const Vector3 lightPos(-20, 23, -20);
+	const Vector3 lightLookat(0, 2, 0);
+	GetMainLight().SetPosition(lightPos);
+	GetMainLight().SetDirection((lightLookat - lightPos).Normal());
+
+	return true;
+}
+
+
+void cViewer::OnUpdate(const float deltaSeconds)
+{
+	__super::OnUpdate(deltaSeconds);
+}
+
+
+void cViewer::OnRender(const float deltaSeconds)
+{
+	__super::OnRender(deltaSeconds);
+}
+
+
+void cViewer::OnEventProc(const sf::Event &evt)
+{
+	ImGuiIO& io = ImGui::GetIO();
+	switch (evt.type)
+	{
+	case sf::Event::KeyPressed:
+		switch (evt.key.code)
+		{
+		case sf::Keyboard::Escape: close(); break;
+		}
+		break;
+	}
+}
---
+++ Src/Test/DX11Ocean5/DX11Ocean5/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#include "../../../../../Common/Common/common.h"
+using namespace common;
+#include "../../../../../Common/Graphic11/graphic11.h"
+#include "../../../../../Common/Framework11/framework11.h"
+
+
+class cViewer;
+extern cViewer *g_viewer;
